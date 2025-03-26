5cb96f7668bc452e7e4f25377fe7b9840f0d52d1
Matej Stupljanec <matej.stupljanec@fer.unizg.hr>
2025-03-26 17:34:18 +0100

    Dodane kategorije i proizvodi

---
+++ scripts/data.js
@@ -0,0 +1,215 @@
+const data = {
+    "website": "Igraonica",
+    "categories": [
+        { 
+            "name" : "Društvene igre",
+            "image" : "meat.jpg",
+            "products" : [
+                { 
+                    "name" : "Monopoly", "image" : "drumstick250g.jpg"
+                },
+                { 
+                    "name" : "Čovječe, ne ljuti se", "image" : "rumpsteak1kg.jpg"
+                },                
+                { 
+                    "name" : "Šah", "image" : "sausages150gr.jpg"
+                },                
+                { 
+                    "name" : "Twister", "image" : "sausages150gr.jpg"
+                },
+                { 
+                    "name" : "Zmije i ljestve", "image" : "sausages150gr.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Karte",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "UNO", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "DOS", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Tarot karte", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Poker karte crne", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Poker karte bijele", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Lopte",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Lopta za nogomet", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lopta za rukomet", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lopta za košarku", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lopta za vaterpolo", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lopta za odbojku", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Plišane igračke",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Medvjedić", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Slon", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lav", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Žirafa", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Pingvin", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Igračke za pijesak",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Kamion", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Bager", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Valjak", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Kantica", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Lopatica", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Igračke na daljinski",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Auto", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Policijski auto", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Formula", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Vatrogasni kamion", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Helikopter", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Puzzle",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Priroda 100 komada", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Priroda 1000 komada", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Karta svijeta 1000 komada", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Životinje 150 komada", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Auto 100 komada", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Igre za van",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Bicikl", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Trampolin", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Ljuljačka", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Vrtuljak", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Tobogan", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Crtanje",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Bojanka", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Flomasteri", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Set za bojanje", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Tempere", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Papir za crtanje", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        },
+        { 
+            "name" : "Figurice",
+            "image" : "candy.jpg",
+            "products" : [
+                { 
+                    "name" : "Super-Man", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Batman", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Mickey Mouse", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Iron Man", "image" : "zivotinjskocarstvo.jpg"
+                },
+                { 
+                    "name" : "Barbie", "image" : "zivotinjskocarstvo.jpg"
+                }
+            ]
+        }
+    ]
+}
